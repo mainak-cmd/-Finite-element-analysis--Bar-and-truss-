9bc8ec255fc3c8c7f64244d8517ad0159f493458
mainak-cmd <76730011+mainak-cmd@users.noreply.github.com>
2021-01-04 14:19:25 +0530

    ##Finite element analysis -Bar and truss##
    elements are initialized to 0

---
+++ fem.py
@@ -0,0 +1,49 @@
+           ##Finite element analysis -Bar and truss##
+
+import numpy as np
+##input of material properties and no of nodes are considered in object to prepare model##
+N= int(input("enter the total number of nodes :"))
+E =[]
+l =[]
+A =[]
+for x in range(N -1) :
+     E.append(int(input("Elastic modulas (N/mm2) of %d element:" %x)))
+     l.append(int(input("length (mm) of %d element:" %x)))
+     A.append(int(input("Cross section (mm2) of %d element:" %x)))
+
+##Element lavel stifness matrix##          
+x=[]  
+j= 0
+k=0
+r=0
+my_E = np.asarray(E)
+my_l = np.asarray(l)
+my_A = np.asarray(A)
+
+for j in my_E: 
+    for k in my_l:
+        for r in my_A:
+            c = j*k/r  # formula of linear stifness 
+    x.append(c)
+
+arr2 = np.array([[1, -1], [-1 ,1]])   #unit matrix
+V=[]
+for i in x:
+    v=i*arr2    #element matrix
+    V.append(v)
+for i in range(len(V)):
+    print("element stifness matrix of",i,":",V[i])
+
+#####Global stifness Matrix####inprogress#####
+G_Mat = [ [ 0 for i in range(N) ] for j in range(N) ] 
+
+
+
+
+
+      
+
+
+
+
+
